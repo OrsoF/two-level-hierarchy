bbf76b037ed0b6fcd1a55141444a493bba1a1b7c
OrsoF <orso.forghieri@gmail.com>
2022-09-08 18:18:23 +0200

    4 rooms environment

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-from env import rooms, tandem
+from envs.rooms import Rooms
+from solvers.mdptoolbox_solver import solve
 
-solver_name = 'mdptoolbox'
-env_name = '4rooms'
-algorithm_name = 'hierarchy'
-
-assert solver in ['mdptoolbox', 'marmot']
-assert env in ['4rooms', 'tandemqueue']
-assert algorithm in ['hierarchy', 'policy_iteration', 'value_iteration']
-
+env = Rooms()
+vi = solve(env.P, env.R)
---
+++ solvers/mdptoolbox_solver.py
@@ -0,0 +1,32 @@
+import mdptoolbox
+import numpy as np
+
+
+def solve(p, r, gamma=0.99, solver='value_iteration', verbose=False):
+    if solver == 'value_iteration':
+        vi = mdptoolbox.mdp.ValueIteration(p, r, gamma)
+    if verbose:
+        vi.setVerbose()
+    vi.run()
+    return vi
+
+
+def action_to_cardinal_direction(a):
+    if a == 0:
+        return 'N'
+    elif a == 1:
+        return 'S'
+    elif a == 2:
+        return 'E'
+    else:
+        return 'W'
+
+
+def show_policy_matrix(policy):
+    policy = [action_to_cardinal_direction(a) for a in policy]
+    mat = np.zeros((10, 10), dtype=str)
+    for i in range(10):
+        for j in range(10):
+            mat[i, j] = policy[10 * i + j]
+
+    print(mat)
